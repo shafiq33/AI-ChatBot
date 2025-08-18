038dbceff9b86186991cb48e49afc70fc39204bb
Huzaifa <huzaifashafiq04@gmail.com>
2025-08-17 22:28:13 -0400

    Updated utils and train

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,4 +1,5 @@
 import nltk
+import numpy as np
 #nltk.download('punkt_tab')
 
 from nltk.stem.porter import PorterStemmer
@@ -13,7 +14,13 @@ def stem(word):
 
 def bag_of_words(tokenized_sentence, all_words):
     pass
-    
-words = ["Organize", "organizes", "organizing"]
-stemmed_words = [stem(w) for w in words]
-print(stemmed_words)
+
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for idx, w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1.0
+
+    return bag
+
+
